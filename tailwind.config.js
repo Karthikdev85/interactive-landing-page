b2f859f23cf4856d38bc784e8632a0f67305f572
Karthiheyan <davidkarthick85@gmail.com>
2022-05-17 10:12:25 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      sm: "400px",
+    },
+
+    extend: {
+      colors: {
+        "almost-white": "#fafafa",
+        "medium-gray": "#696969",
+        "almost-black": "#141414",
+      },
+    },
+  },
+  plugins: [],
+};
